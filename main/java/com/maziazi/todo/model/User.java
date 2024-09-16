d86318cf50d8994f28b40b1151c8fc1818b23a7c
maziazi <mazis9651@gmail.com>
2024-09-16 17:34:11 +0700

    fix:error

--- main/java/com/maziazi/todo/model/User.java
+++ main/java/com/maziazi/todo/model/User.java
@@ -1,2 +1,42 @@
-package com.maziazi.todo.model;public class User {
+package com.maziazi.todo.model;
+
+import lombok.AllArgsConstructur;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String description;
+/*
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String userName;
+
+   private String password;
+*/
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    @JsonIgnore
+    private User user;
+
+    @OneToMany (mappedBy = "category", fetch = FetchType.EAGER)
+    private List<Todo> todoList;
 }
